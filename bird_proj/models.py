c059c59122b45fcadab966ed96297a7dbec048f7
Nicholas Walton <ngwalton@gmail.com>
2020-03-12 23:46:41 -0500

    Added models, listed bird_proj in installed apps and ran migration

--- bird_proj/models.py
+++ bird_proj/models.py
@@ -1,3 +1,28 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+# need additional tables for type and minute
+class Bird(models.Model):
+    obs_id = models.AutoField(primary_key=True, db_index=True, editable=False, help_text="Observation id")
+    entry_id = models.ForeignKey("Site", on_delete=models.CASCADE, help_text="Foreign key on Site entry_id")
+    spec_id = models.ForeignKey("Alpha", on_delete=models.PROTECT, help_text="Foreign key on Alpha spec (alpha code)")
+    type_choices = [("S", "singing"), ("C", "calling"), ("O", "observed"), ("F", "flyover")]
+    type = models.CharField(max_length=1, choices=type_choices, null=False, help_text="Type of detection")
+    minute = models.PositiveSmallIntegerField(null=False, help_text="Minute of first detection")
+
+# need additional tables for observer, sid?, entry, sky, and tempc
+class Site(models.Model):
+    entry_id = models.AutoField(primary_key=True, db_index=True, editable=False)
+    observer = models.CharField(max_length=200, null=False, help_text="Name of observer")
+    sid = models.CharField(max_length=10, null=False, help_text="Site id")
+    day = models.DateTimeField(null=False, help_text="Date of count")
+    entry = models.PositiveSmallIntegerField(null=False, help_text="First or second entry")
+    sky = models.PositiveSmallIntegerField(null=False, help_text="Sky condition")
+    tempc = models.PositiveSmallIntegerField(null=False, help_text="Temperature degrees C")
+
+class Alpha(models.Model):
+    spec_id = models.AutoField(primary_key=True, db_index=True, editable=False)
+    spec = models.CharField(max_length=4,  null=False, unique=True, help_text="Four letter alpha code")
+    common = models.CharField(max_length=200, null=False, unique=True, help_text="Common name")
+    sci = models.CharField(max_length=200, null=False, unique=True, help_text="Scientific name")
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bird_proj.apps.BirdProjConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
