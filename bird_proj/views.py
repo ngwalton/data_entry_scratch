b0efe076a0bfc7e86960aa2204e587abc3c168c4
Nicholas Walton <ngwalton@gmail.com>
2020-04-07 07:42:42 -0500

    Added site to form

--- bird_proj/views.py
+++ bird_proj/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, redirect
 from .models import Site, Bird
-from django.forms import inlineformset_factory
+from django.forms import inlineformset_factory, modelform_factory
 
 def index(request, pk):
     site = Site.objects.get(entry_id=pk)
     BirdFormset = inlineformset_factory(Site, Bird, exclude=(), extra=10)
+    SiteForm = modelform_factory(Site, exclude=())
 
     if request.method == 'POST':
+        site_form = SiteForm(request.POST, instance=site)
         formset = BirdFormset(request.POST, instance=site)
-        if formset.is_valid():
+        if formset.is_valid() and site_form.is_valid():
+            site_form.save()
             formset.save()
 
             return redirect('index', pk=site.pk)
 
     formset = BirdFormset(instance=site)
+    site_form = SiteForm(instance=site)
 
-    return render(request, 'bird_proj/index.html', {'formset' : formset})
+    return render(request, 'bird_proj/index.html',
+        {'formset' : formset, 'site_form':site_form})
